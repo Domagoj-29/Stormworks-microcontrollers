2de3d286533ee9c1ce6c18328597379195fa6406
Domagoj29 <domagojradulovic@yahoo.com>
2026-08-28 12:12:28 +0300

    Update GPS V10.lua
    The getBackgroundColor function now actually gets used.

--- GPS V10.lua
+++ GPS V10.lua
@@ -97,8 +97,8 @@ end
 local function round(x)
 	return math.floor(x + 0.5)
 end
-local function getBackgroundColor(i)
-	return {20, 20, 20, 255 - i / (h - 2) * 50}
+local function getBackgroundColor(y)
+	return {20, 20, 20, 255 - y / (h - 2) * 50}
 end
 
 -- onTick functions
@@ -457,7 +457,7 @@ function onDraw()
 		end
 	elseif ScreenMode == "Data" then
 		for i = 1, h - 2 do
-			screen.setColor(20, 20, 20, 255 - i / (h - 2) * 50)
+			screen.setColor(getBackgroundColor(i))
 			screen.drawRectF(1, i, w - 2, 1)
 		end
 
